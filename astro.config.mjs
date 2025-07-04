3781db3d65075c181cf9c2d3b4742b47a37f9910
chineseonmill <huixiang.liu09@gmail.com>
2025-07-04 09:57:14 -0700

    edit content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site:'https://chineseonmill.github.io/',
     output: 'static',
+    integrations: [
+        tailwind(),
+        sitemap(),
+      ],
+      markdown: {
+        shikiConfig: {
+          theme: 'github-light',
+          wrap: true
+        }
+      }
 });
